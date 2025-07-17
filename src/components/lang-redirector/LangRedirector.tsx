375cf5a6dbe250dffd860ca04b9ae281c8b6f81e
Dmitry Makarov <makdmt@gmail.com>
2025-07-17 19:00:44 +0300

    feat(components) add logic to LangRedirector

--- src/components/lang-redirector/LangRedirector.tsx
+++ src/components/lang-redirector/LangRedirector.tsx
@@ -1,10 +1,23 @@
-import type { FC } from "react";
-import { Navigate, Outlet,useParams } from "react-router-dom";
+import type {FC} from "react";
+import {Navigate, Outlet, useLocation, useParams} from "react-router-dom";
+
+import {LocaleService} from "@/lib/locale-service.ts";
 
 export const LangRedirector: FC = () => {
 
-    const { lang } = useParams();
-    console.log(lang);
+    const {lang} = useParams();
+    const location = useLocation();
+    const localeService = new LocaleService(lang, location.search);
+    const bestSupportedLocale = localeService.findBestSupportedLocale();
 
-    return <Outlet />;
+    const segments = location.pathname.split('/').filter(Boolean);
+    segments[0] = bestSupportedLocale;
+    const newPathname = '/' + segments.join('/');
+
+    if (bestSupportedLocale === lang) return <Outlet/>
+    else {
+        localeService.setBcpTagToLocalStorage(bestSupportedLocale);
+        return <Navigate to={`${newPathname}${location.search}${location.hash}`} replace/>
+    }
 }
+

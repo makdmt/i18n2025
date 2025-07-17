c2560fc3a00c7c231eaf3236a050505cf792ad72
Dmitry Makarov <makdmt@gmail.com>
2025-07-17 18:20:15 +0300

    feat(services) add logic to LocaleService

--- src/lib/locale-service.ts
+++ src/lib/locale-service.ts
@@ -1,20 +1,38 @@
-import {SUPPORTED_LANGS, SUPPORTED_LOCALES, DEFAULT_LANG, DEFAULT_LOCALE} from "@/constants";
-import {LANG_COOKIE_NAME} from "@/constants/lang-cookie-name.ts";
 import Cookies from 'js-cookie';
+
+import {DEFAULT_LANG, DEFAULT_LOCALE, SUPPORTED_LANGS, SUPPORTED_LOCALES} from "@/constants";
+import {LANG_COOKIE_NAME} from "@/constants/lang-cookie-name.ts";
 import {geoService} from "@/lib/geo-service.ts";
+import type {Lang} from "@/types";
 
 const LANG_LOCALSTORAGE_KEY = 'lang';
 
-interface ILocaleService {
-    getCurrentRegion: (queryString: string) => string;
-}
+export class LocaleService {
+    requestedLocale: string;
+    queryString: string;
 
-export class LocaleService implements ILocaleService {
-
-    constructor(bcpTag: string) {
+    constructor(bcpTag: string, queryString: string) {
+        this.queryString = queryString;
+        if (this.isValidBcpTag(bcpTag)) {
+            this.requestedLocale = bcpTag;
+            return;
+        } else {
+            let savedLocale: string | null | undefined = this.getLocalStorageBcpTag();
+            if (!this.isValidBcpTag(savedLocale)) savedLocale = this.getCookieBcpTag();
+            if (!this.isValidBcpTag(savedLocale)) savedLocale = this.getBrowserBcpTag();
+            if (!savedLocale) savedLocale = this.getDefaultLocale();
+            this.requestedLocale = savedLocale;
+        }
     }
 
-    isValidBcpTag(bcpTag: string) {
+    setBestSupportedLocale() {
+        const locale = this.findBestSupportedLocale();
+        this.setBcpTagToLocalStorage(locale);
+        return locale;
+    }
+
+    isValidBcpTag(bcpTag: unknown): bcpTag is string {
+        if (typeof bcpTag !== 'string' || !bcpTag) return false;
         try {
             new Intl.Locale(bcpTag);
             return true;
@@ -31,8 +49,11 @@ export class LocaleService implements ILocaleService {
         return locale.language;
     }
 
-    isLangSupported(lang: string) {
-        return (SUPPORTED_LANGS as readonly string[]).includes(lang);
+    isLangValidAndSupported(bcpTag: unknown): bcpTag is Lang {
+        if (!this.isValidBcpTag(bcpTag)) return false;
+        const locale = this.getIntlLocaleFromBcpTag(bcpTag);
+        const validatedLang = this.getLangFromIntlLocale(locale);
+        return (SUPPORTED_LANGS as readonly string[]).includes(validatedLang);
     }
 
     getCookieBcpTag() {
@@ -47,12 +68,16 @@ export class LocaleService implements ILocaleService {
         return DEFAULT_LANG;
     }
 
-    getDefaultLocale() {
-        return DEFAULT_LOCALE;
+    findBestSupportedLang() {
+        let requestedLocale: string | null | undefined = this.requestedLocale;
+        if (!this.isLangValidAndSupported(requestedLocale)) requestedLocale = this.getCookieBcpTag();
+        if (!this.isLangValidAndSupported(requestedLocale)) requestedLocale = this.getBrowserBcpTag();
+        if (!this.isLangValidAndSupported(requestedLocale)) requestedLocale = this.getDefaultLang();
+        return requestedLocale as Lang;
     }
 
     getLocalStorageBcpTag() {
-        localStorage.getItem(LANG_LOCALSTORAGE_KEY);
+        return localStorage.getItem(LANG_LOCALSTORAGE_KEY);
     }
 
     setBcpTagToLocalStorage(bcpTag: string) {
@@ -63,38 +88,29 @@ export class LocaleService implements ILocaleService {
         return locale.region;
     }
 
-    findBestSupportedLang(preferredLang: string) {
-
+    getCurrentUserRegion() {
+        return geoService.getCurrentRegion(this.queryString);
     }
 
-    findBestSupportedLocale(lang: string, region: string) {
+    findLocaleByRegionAndSupportedLang(lang: Lang, region: string) {
+        const supportedLangRegionLocale = SUPPORTED_LOCALES.find(locale => {
+            const intlLocale = this.getIntlLocaleFromBcpTag(locale);
+            const localeLang = this.getLangFromIntlLocale(intlLocale);
+            const localeRegion = this.getRegionFromIntlLocale(intlLocale);
+            return localeLang === lang && localeRegion === region;
+        });
 
+        if (supportedLangRegionLocale) return supportedLangRegionLocale;
+        else return lang;
     }
 
-
-    getCurrentUserRegion(queryString: string) {
-        return geoService.getCurrentRegion(queryString);
+    findBestSupportedLocale() {
+        const lang = this.findBestSupportedLang();
+        const region = this.getCurrentUserRegion();
+        return this.findLocaleByRegionAndSupportedLang(lang, region);
     }
 
-
-    makeBcpTag(lang: string, region?: string) {
-        if (!region) return lang;
-        return `${lang}-${region}`;
+    getDefaultLocale() {
+        return DEFAULT_LOCALE;
     }
-
-    isLocaleSupported(locale: Intl.Locale) {
-        const validatedLocale = this.makeBcpTag(locale.language, locale.region);
-        return (SUPPORTED_LOCALES as readonly string[]).includes(validatedLocale);
-    }
-
-
-    getRegionFromBcpTab(bcpTag: string) {
-
-    }
-
-    isUserRegionSupported(region: string) {
-        return
-    }
-
-
 }

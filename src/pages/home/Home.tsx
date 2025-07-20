f79eea35dc231a43bb1c9ec0cf6b24571654bc25
Dmitry Makarov <makdmt@gmail.com>
2025-07-20 12:10:05 +0300

    feat(pages) locolize HomePage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import { type FC } from "react";
+import { FormattedDate, FormattedMessage, FormattedNumber } from "react-intl";
 import { Link } from "react-router-dom";
 
 import articleAr from "@/assets/article-ar.jpg";
@@ -9,22 +10,25 @@ import articleL10nRu from "@/assets/article-l10n-ru.jpg";
 import articleRtlIcons from "@/assets/article-rtl-icons.jpg";
 import articleUiBy from "@/assets/article-ui-by.jpg";
 import { Layout } from "@/components";
+import { PageIntlProvider } from "@/components/page-intl-provider";
+import { useLocale } from "@/hooks/useLocale";
+import type { PAGE_TRANSLATION_KEYS } from "@/page-translation-keys";
 import type { Locale } from "@/types";
 
 import styles from "./styles.module.css";
 
+const PAGE_NAME: keyof typeof PAGE_TRANSLATION_KEYS = 'homePage';
+
 const ARTICLES = [
     {
-        title: "Какие иконки нужно разворачивать для RTL, а какие — нет?",
-        description:
-            "Не все иконки требуют зеркального отражения при переключении на RTL-языки. Разбираемся, какие иконки зависят от направления текста, а какие — универсальны",
+        title: <FormattedMessage id="homePage.rtlArticle.title" defaultMessage="Какие иконки нужно разворачивать для RTL, а какие — нет?" description="заголовок статьи rtl иконок" />,
+        description: <FormattedMessage id="homePage.rtlArticle.description" defaultMessage="Не все иконки требуют зеркального отражения при переключении на RTL-языки. Разбираемся, какие иконки зависят от направления текста, а какие — универсальны" description="краткое содержание статьи rtl иконок" />,
         imageUrl: articleRtlIcons,
         articleLink: "article/rtl-icons",
     },
     {
-        title: "Логические CSS-свойства в интерфейсах с поддержкой i18n",
-        description:
-            "Узнайте, как логические CSS-свойства помогают создавать адаптивные интерфейсы для разных языков и направлений письма — без усложнения кода и дублирования стилей.",
+        title: <FormattedMessage id="homePage.cssArticle.title" defaultMessage="Логические CSS-свойства в интерфейсах с поддержкой i18n" description="заголовок статьи css" />,
+        description: <FormattedMessage id="homePage.cssArticle.description" defaultMessage="Узнайте, как логические CSS-свойства помогают создавать адаптивные интерфейсы для разных языков и направлений письма — без усложнения кода и дублирования стилей." description="краткое содержание статьи css" />,
         imageUrl: articleCss,
         articleLink: "article/css",
     },
@@ -35,36 +39,32 @@ const getRegionArticleByLocale = (locale: Locale) => {
         case "ru":
         case "ru-RU":
             return {
-                title: "Как адаптировать веб-приложение под российских пользователей: нюансы локализации",
-                description:
-                    "Изучаем предпочтения русскоязычных пользователей, числовые и валютные форматы, перевод интерфейса и юридические аспекты (например, закон о персональных данных)",
+                title: <FormattedMessage id="homePage.ruArticle.title" defaultMessage="Как адаптировать веб-приложение под российских пользователей: нюансы локализации" description="заголовок статьи для рус региона" />,
+                description: <FormattedMessage id="homePage.ruArticle.description" defaultMessage="Изучаем предпочтения русскоязычных пользователей, числовые и валютные форматы, перевод интерфейса и юридические аспекты (например, закон о персональных данных)" description="краткое содержание статьи для рус региона" />,
                 imageUrl: articleL10nRu,
                 articleLink: "article/l10n-ru",
             };
 
         case "ru-BY":
             return {
-                title: "Двухъязычный интерфейс: как учесть русский и белорусский языки в одном продукте",
-                description:
-                    "Рассматриваем подходы к реализации двуязычного интерфейса, стандарты перевода и культурные отличия. Особое внимание — контенту на белорусском языке",
+                title: <FormattedMessage id="homePage.byArticle.title" defaultMessage="Двухъязычный интерфейс: как учесть русский и белорусский языки в одном продукте" description="заголовок статьи для by региона" />,
+                description: <FormattedMessage id="homePage.byArticle.description" defaultMessage="Рассматриваем подходы к реализации двуязычного интерфейса, стандарты перевода и культурные отличия. Особое внимание — контенту на белорусском языке" description="краткое содержание статьи для by региона" />,
                 imageUrl: articleUiBy,
                 articleLink: "article/ui-by",
             };
 
         case "ru-KZ":
             return {
-                title: "Русский и казахский: эффективная локализация для Казахстана",
-                description:
-                    "Разбираем сценарии, когда приложение должно быть доступно сразу на двух языках, и особенности казахской локали (в т.ч. поддержка латиницы и кириллицы, особенности форматов дат)",
+                title: <FormattedMessage id="homePage.kzArticle.title" defaultMessage="Русский и казахский: эффективная локализация для Казахстана" description="заголовок статьи для kz региона" />,
+                description: <FormattedMessage id="homePage.kzArticle.description" defaultMessage="Разбираем сценарии, когда приложение должно быть доступно сразу на двух языках, и особенности казахской локали (в т.ч. поддержка латиницы и кириллицы, особенности форматов дат)" description="краткое содержание статьи для kz региона" />,
                 imageUrl: articleI18nKz,
                 articleLink: "article/i18n-kz",
             };
 
         case "ar":
             return {
-                title: "Локализация для арабоязычного мира: RTL, форматы и культурные коды",
-                description:
-                    "От адаптации интерфейса под направление письма справа налево до выбора правильных формулировок — ключевые аспекты локализации для стран Ближнего Востока и Северной Африки",
+                title: <FormattedMessage id="homePage.arArticle.title" defaultMessage="Локализация для арабоязычного мира: RTL, форматы и культурные коды" description="заголовок статьи для ar региона" />,
+                description: <FormattedMessage id="homePage.arArticle.description" defaultMessage="От адаптации интерфейса под направление письма справа налево до выбора правильных формулировок — ключевые аспекты локализации для стран Ближнего Востока и Северной Африки" description="краткое содержание статьи для ar региона" />,
                 imageUrl: articleAr,
                 articleLink: "article/ar",
             };
@@ -72,9 +72,8 @@ const getRegionArticleByLocale = (locale: Locale) => {
         case "en":
         default:
             return {
-                title: "Проектирование для глобальной аудитории: английский как универсальный язык",
-                description:
-                    "Почему английский часто используется как язык по умолчанию в международных приложениях и как писать интерфейсные тексты, которые будут понятны, культурно нейтральны и удобны для последующей локализации",
+                title: <FormattedMessage id="homePage.enArticle.title" defaultMessage="Локализация для арабоязычного мира: RTL, форматы и культурные коды" description="заголовок статьи для ar региона" />,
+                description: <FormattedMessage id="homePage.enArticle.description" defaultMessage="От адаптации интерфейса под направление письма справа налево до выбора правильных формулировок — ключевые аспекты локализации для стран Ближнего Востока и Северной Африки" description="краткое содержание статьи для ar региона" />,
                 imageUrl: articleEn,
                 articleLink: "article/en",
             };
@@ -82,104 +81,173 @@ const getRegionArticleByLocale = (locale: Locale) => {
 };
 
 export const Home: FC = () => {
+
+    const { locale } = useLocale();
+
     const { title, description, imageUrl, articleLink } =
-        getRegionArticleByLocale("ru");
+        getRegionArticleByLocale(locale);
 
     return (
-        <Layout>
-            <main className={styles.content}>
-                <section className={styles.hero}>
-                    <h1 className={styles.heroTitle}>
-                        Соединяем цифровые миры на всех языках
-                    </h1>
+        <PageIntlProvider pageName={PAGE_NAME}>
+            <Layout>
+                <main className={styles.content}>
+                    <section className={styles.hero}>
+                        <h1 className={styles.heroTitle}>
+                            <FormattedMessage
+                                id="homePage.hero.title"
+                                defaultMessage="Соединяем цифровые миры на всех языках"
+                                description="слоган мероприятия"
+                            />
+                        </h1>
 
-                    <div className={styles.heroDetails}>
-                        <span className={styles.heroDetailsItem}>
-                            Конференция I&L-2025
-                        </span>
+                        <div className={styles.heroDetails}>
+                            <span className={styles.heroDetailsItem}>
+                                <FormattedMessage
+                                    id="homePage.hero.conference"
+                                    defaultMessage="Конференция I&L-{year}"
+                                    description="слоган мероприятия"
+                                    values={{
+                                        year: <FormattedDate value={new Date('2025-08-15')} year='numeric' />
+                                    }}
+                                />
+                            </span>
 
-                        <span className={styles.heroDetailsItem}>
-                            15 августа 2025 г.
-                        </span>
+                            <span className={styles.heroDetailsItem}>
+                                <FormattedDate
+                                    value={new Date('2025-08-15')}
+                                    year="numeric"
+                                    month="long"
+                                    day="numeric"
+                                />
+                            </span>
 
-                        <span className={styles.heroDetailsItem}>
-                            Москва, Россия
-                        </span>
+                            <span className={styles.heroDetailsItem}>
+                                <FormattedMessage
+                                    id="homePage.hero.location"
+                                    defaultMessage="Москва, Россия"
+                                    description="место проведения мероприятия"
+                                />
+                            </span>
 
-                        <span className={styles.heroDetailsItem}>
-                            35 000,00 ₽ билет
-                        </span>
-                    </div>
-
-                    <a className={styles.heroRegister} href="">
-                        Зарегистрироваться
-                    </a>
-                </section>
-
-                <section className={styles.regionArticle}>
-                    <h2 className={styles.regionArticleTitle}>
-                        Актуально для вашего региона
-                    </h2>
-
-                    <Link className={styles.articleCard} to={articleLink}>
-                        <div className={styles.cardContent}>
-                            <h3 className={styles.cardTitle}>{title}</h3>
-
-                            <p className={styles.cardDescription}>
-                                {description}
-                            </p>
-
-                            <span className={styles.cardRead}>Читать</span>
+                            <span className={styles.heroDetailsItem}>
+                                <FormattedMessage
+                                    id="homePage.hero.price"
+                                    defaultMessage="{price} билет"
+                                    description="стоимость билета"
+                                    values={{
+                                        price: <FormattedNumber
+                                            value={35000}
+                                            style="currency"
+                                            currency="RUB"
+                                            minimumFractionDigits={2}
+                                            maximumFractionDigits={2}
+                                        />
+                                    }}
+                                />
+                            </span>
                         </div>
 
-                        <img className={styles.cardImage} src={imageUrl} />
-                    </Link>
-                </section>
+                        <a className={styles.heroRegister} href="">
+                            <FormattedMessage
+                                id="homePage.hero.register"
+                                defaultMessage="Зарегистрироваться"
+                                description="кнопка регистрации"
+                            />
+                        </a>
+                    </section>
 
-                <section className={styles.articles}>
-                    <h2 className={styles.articlesTitle}>Статьи</h2>
+                    <section className={styles.regionArticle}>
+                        <h2 className={styles.regionArticleTitle}>
+                            <FormattedMessage
+                                id="homePage.regionArticle.title"
+                                defaultMessage="Актуально для вашего региона"
+                                description="заголовок раздела локальных новостей"
+                            />
+                        </h2>
 
-                    {ARTICLES.length > 0 && (
-                        <p className={styles.articlesDescription}>
-                            Всего {ARTICLES.length} статьи
-                        </p>
-                    )}
+                        <Link className={styles.articleCard} to={articleLink}>
+                            <div className={styles.cardContent}>
+                                <h3 className={styles.cardTitle}>{title}</h3>
 
-                    <div className={styles.articlesList}>
-                        {ARTICLES.map(
-                            (
-                                { title, description, imageUrl, articleLink },
-                                index
-                            ) => (
-                                <Link
-                                    key={index}
-                                    className={styles.articleCard}
-                                    to={articleLink}
-                                >
-                                    <div className={styles.cardContent}>
-                                        <h3 className={styles.cardTitle}>
-                                            {title}
-                                        </h3>
+                                <p className={styles.cardDescription}>
+                                    {description}
+                                </p>
 
-                                        <p className={styles.cardDescription}>
-                                            {description}
-                                        </p>
-
-                                        <span className={styles.cardRead}>
-                                            Читать
-                                        </span>
-                                    </div>
-
-                                    <img
-                                        className={styles.cardImage}
-                                        src={imageUrl}
+                                <span className={styles.cardRead}>
+                                    <FormattedMessage
+                                        id="homePage.article.read"
+                                        defaultMessage="Читать"
+                                        description="кнопка для перехода к полному тексту статьи"
                                     />
-                                </Link>
-                            )
+                                </span>
+                            </div>
+
+                            <img className={styles.cardImage} src={imageUrl} />
+                        </Link>
+                    </section>
+
+                    <section className={styles.articles}>
+                        <h2 className={styles.articlesTitle}>
+                            <FormattedMessage
+                                id="homePage.articles.title"
+                                defaultMessage="Статьи"
+                                description="заголовок разела со списком статей"
+                            />
+                        </h2>
+
+                        {ARTICLES.length > 0 && (
+                            <p className={styles.articlesDescription}>
+                                <FormattedMessage
+                                    id="homePage.articles.description"
+                                    defaultMessage="Всего {count, plural, one {# статья} few {# статьи} many {# статей} other {# статьи}}"
+                                    description="количество статей для чтения в списке"
+                                    values={{
+                                        count: ARTICLES.length
+                                    }}
+                                />
+                            </p>
                         )}
-                    </div>
-                </section>
-            </main>
-        </Layout>
+
+                        <div className={styles.articlesList}>
+                            {ARTICLES.map(
+                                (
+                                    { title, description, imageUrl, articleLink },
+                                    index
+                                ) => (
+                                    <Link
+                                        key={index}
+                                        className={styles.articleCard}
+                                        to={articleLink}
+                                    >
+                                        <div className={styles.cardContent}>
+                                            <h3 className={styles.cardTitle}>
+                                                {title}
+                                            </h3>
+
+                                            <p className={styles.cardDescription}>
+                                                {description}
+                                            </p>
+
+                                            <span className={styles.cardRead}>
+                                                <FormattedMessage
+                                                    id="homePage.article.read"
+                                                    defaultMessage="Читать"
+                                                    description="кнопка для перехода к полному тексту статьи"
+                                                />
+                                            </span>
+                                        </div>
+
+                                        <img
+                                            className={styles.cardImage}
+                                            src={imageUrl}
+                                        />
+                                    </Link>
+                                )
+                            )}
+                        </div>
+                    </section>
+                </main>
+            </Layout>
+        </PageIntlProvider>
     );
 };

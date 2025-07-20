f177e9b51d38888cd4c7cebfba715e5915c9bffd
Dmitry Makarov <makdmt@gmail.com>
2025-07-20 13:24:26 +0300

    feat(pages) localize ArticleEn page

--- src/pages/article-en/ArticleEn.tsx
+++ src/pages/article-en/ArticleEn.tsx
@@ -1,24 +1,40 @@
 import { type FC } from "react";
+import { FormattedMessage } from "react-intl";
 
 import { Layout } from "@/components";
+import { PageIntlProvider } from "@/components/page-intl-provider";
+import { PAGE_TRANSLATION_KEYS } from "@/page-translation-keys";
 
 import styles from "./styles.module.css";
 
-export const ArticleEn: FC = () => (
-    <Layout>
-        <main className={styles.article}>
-            <h1>
-                Проектирование для глобальной аудитории: английский как
-                универсальный язык
-            </h1>
+const PAGE_NAME: keyof typeof PAGE_TRANSLATION_KEYS = 'articleEn';
 
-            <p>
-                Английский язык часто используется в интерфейсах как
-                универсальный, особенно на этапе MVP или при работе на
-                международный рынок. Мы делимся рекомендациями, как писать
-                интерфейсные тексты, которые останутся понятными, нейтральными и
-                легко поддающимися переводу в будущем.
-            </p>
-        </main>
-    </Layout>
+export const ArticleEn: FC = () => (
+    <PageIntlProvider pageName={PAGE_NAME}>
+        <Layout>
+            <main className={styles.article}>
+                <h1>
+                    <FormattedMessage
+                        id="articleEn.title"
+                        defaultMessage="Проектирование для глобальной аудитории: английский как
+                универсальный язык"
+                        description="заголовок статьи"
+                    />
+                </h1>
+
+                <p>
+                    <FormattedMessage
+                        id="articleEn.text"
+                        defaultMessage="Английский язык часто используется в интерфейсах как
+                    универсальный, особенно на этапе MVP или при работе на
+                    международный рынок. Мы делимся рекомендациями, как писать
+                    интерфейсные тексты, которые останутся понятными, нейтральными и
+                    легко поддающимися переводу в будущем."
+                        description="содержание статьи"
+                    />
+
+                </p>
+            </main>
+        </Layout>
+    </PageIntlProvider>
 );

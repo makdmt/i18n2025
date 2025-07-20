59a454630d3a9caea886c692a1298f97b24c12f1
Dmitry Makarov <makdmt@gmail.com>
2025-07-20 14:37:56 +0300

    feat(hooks) add useLocale hook

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,14 +1,14 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-import {DEFAULT_LOCALE} from "@/constants";
-
-import translations from "../../translations.json";
+import { DEFAULT_LOCALE } from "@/constants";
+import type { Lang, Locale } from "@/types";
 
 
 export function useLocale() {
-    const {lang} = useParams();
-    const locale = lang || DEFAULT_LOCALE;
+    const { lang: urllocale } = useParams();
+    const locale = urllocale as Locale || DEFAULT_LOCALE;
+    const lang = new Intl.Locale(locale).language as Lang;
 
-    return {locale, translations}
+    return { locale, lang }
 }
 

e6633a7a5902d81e2dcd75a3b01ab7f661dfe6e3
Dmitry Makarov <makdmt@gmail.com>
2025-07-20 12:15:55 +0300

    feat(pages) localize ArticleAr page

--- src/pages/article-ar/ArticleAr.tsx
+++ src/pages/article-ar/ArticleAr.tsx
@@ -1,24 +1,40 @@
 import { type FC } from "react";
+import { FormattedMessage } from "react-intl";
 
 import { Layout } from "@/components";
+import { PageIntlProvider } from "@/components/page-intl-provider";
+import { PAGE_TRANSLATION_KEYS } from "@/page-translation-keys";
 
 import styles from "./styles.module.css";
 
-export const ArticleAr: FC = () => (
-    <Layout>
-        <main className={styles.article}>
-            <h1>
-                Локализация для арабоязычного мира: RTL, форматы и культурные
-                коды
-            </h1>
+const PAGE_NAME: keyof typeof PAGE_TRANSLATION_KEYS = 'articleAr';
 
-            <p>
-                Интерфейсы на арабском языке требуют переосмысления привычного
-                порядка элементов: направление письма меняется на RTL. В статье
-                мы объясняем, как грамотно адаптировать верстку, типографику и
-                иконки, чтобы интерфейс выглядел естественно для арабоязычных
-                пользователей, и при этом оставался универсальным.
-            </p>
-        </main>
-    </Layout>
+export const ArticleAr: FC = () => (
+    <PageIntlProvider pageName={PAGE_NAME}>
+        <Layout>
+            <main className={styles.article}>
+                <h1>
+                    <FormattedMessage
+                        id="articleAr.title"
+                        defaultMessage="Локализация для арабоязычного мира: RTL, форматы и культурные
+                коды"
+                        description="заголовок ar статьи"
+                    />
+                </h1>
+
+                <p>
+                    <FormattedMessage
+                        id="articleAr.text"
+                        defaultMessage="Интерфейсы на арабском языке требуют переосмысления привычного
+                    порядка элементов: направление письма меняется на RTL. В статье
+                    мы объясняем, как грамотно адаптировать верстку, типографику и
+                    иконки, чтобы интерфейс выглядел естественно для арабоязычных
+                    пользователей, и при этом оставался универсальным."
+                        description="Содержимое ar статьи"
+                    />
+
+                </p>
+            </main>
+        </Layout>
+    </PageIntlProvider>
 );

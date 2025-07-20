7021f09b177b6c06745c294e245c68d290e65f75
Dmitry Makarov <makdmt@gmail.com>
2025-07-20 13:19:49 +0300

    feat(pages) localize ArticleRtlIcons page

--- src/pages/article-rtl-icons/ArticleRtlIcons.tsx
+++ src/pages/article-rtl-icons/ArticleRtlIcons.tsx
@@ -1,7 +1,9 @@
 import cn from "classnames";
 import { type FC } from "react";
+import { FormattedMessage } from "react-intl";
 
 import { Layout } from "@/components";
+import { PageIntlProvider } from "@/components/page-intl-provider";
 import {
     ClockIcon,
     GamepadIcon,
@@ -10,93 +12,161 @@ import {
     PaperNoteIcon,
     WindowIcon,
 } from "@/icons";
+import type { PAGE_TRANSLATION_KEYS } from "@/page-translation-keys";
 
 import styles from "./styles.module.css";
 
+const PAGE_NAME: keyof typeof PAGE_TRANSLATION_KEYS = 'articleRtlIcons'
+
 export const ArticleRtlIcons: FC = () => (
-    <Layout>
-        <main className={styles.article}>
-            <h1>
-                Какие иконки нужно разворачивать для RTL, <br /> а какие — нет?
-            </h1>
-
-            <p>
-                При адаптации интерфейсов под языки с письмом справа налево
-                (RTL) важно учитывать не только верстку и тексты, но и
-                визуальные элементы. Многие иконки требуют зеркального
-                отражения, а другие должны оставаться в оригинальном виде.
-                Неправильное отображение может сделать интерфейс менее
-                интуитивным и запутать пользователя.
-            </p>
-
-            <section className={styles.section}>
-                <h2>Почему важно разворачивать иконки в RTL?</h2>
+    <PageIntlProvider pageName={PAGE_NAME}>
+        <Layout>
+            <main className={styles.article}>
+                <h1>
+                    <FormattedMessage
+                        id="articleRtlIcons.title"
+                        defaultMessage="Какие иконки нужно разворачивать для RTL, {br} а какие — нет?"
+                        description="речь о пиктограммах в интефейсе"
+                        values={{
+                            br: <br />,
+                        }}
+                    />
+                </h1>
 
                 <p>
-                    В RTL-интерфейсах меняется не только направление текста, но
-                    и логика взаимодействия. Иконки с явной направленностью
-                    должны отражать это изменение, чтобы сохранить естественный
-                    поток восприятия и соответствовать ожиданиям пользователя.
-                </p>
-            </section>
+                    <FormattedMessage
+                        id="articleRtlIcons.intro"
+                        defaultMessage="При адаптации интерфейсов под языки с письмом справа налево
+                    (RTL) важно учитывать не только верстку и тексты, но и
+                    визуальные элементы. Многие иконки требуют зеркального
+                    отражения, а другие должны оставаться в оригинальном виде.
+                    Неправильное отображение может сделать интерфейс менее
+                    интуитивным и запутать пользователя."
+                        description="вступление к статье"
+                    />
 
-            <section className={styles.section}>
-                <h2>Иконки, которые нужно разворачивать</h2>
-
-                <p>
-                    Разворачивайте иконки, которые имеют явное направление или
-                    асимметричную форму (за исключением примеров, которые мы
-                    рассмотрим позже в статье). Например, иконка заметок с явным
-                    направлением текста, асимметричная иконка пользовательского
-                    интерфейса окна приложения или иконка сообщения. Примеры
-                    таких иконок:
                 </p>
 
-                <div className={cn(styles.icons)} data-testid="rtl-icons">
-                    <PaperNoteIcon />
-                    <MessageIcon />
-                    <WindowIcon />
-                </div>
-            </section>
+                <section className={styles.section}>
+                    <h2>
+                        <FormattedMessage
+                            id="articleRtlIcons.whyImportant.title"
+                            defaultMessage="Почему важно разворачивать иконки в RTL?"
+                            description="подзакголовок раздела статьи"
+                        />
+                    </h2>
 
-            <section className={styles.section}>
-                <h2>Иконки, которые не нужно разворачивать</h2>
+                    <p>
+                        <FormattedMessage
+                            id="articleRtlIcons.whyImportant.text"
+                            defaultMessage="В RTL-интерфейсах меняется не только направление текста, но
+                        и логика взаимодействия. Иконки с явной направленностью
+                        должны отражать это изменение, чтобы сохранить естественный
+                        поток восприятия и соответствовать ожиданиям пользователя."
+                            description="содеражание раздела статьи"
+                        />
+                    </p>
+                </section>
 
-                <p>
-                    Можно не разворачивать симметричные иконки. Иконки, которые
-                    являются логотипом или представляют бренд тоже не следует
-                    зеркалить — они должны оставаться узнаваемыми. Особое
-                    внимание уделите элементам, которые подчиняются «правилу
-                    правой руки» — если иконка интуитивно ассоциируется с
-                    действием правой руки (как держание предмета), её ориентация
-                    должна сохраняться независимо от направления письма. Примеры
-                    таких иконок:
-                </p>
+                <section className={styles.section}>
+                    <h2>
+                        <FormattedMessage
+                            id="articleRtlIcons.flipIcons.title"
+                            defaultMessage="Иконки, которые нужно разворачивать"
+                            description="подзаголовок раздела статьи"
+                        />
 
-                <div className={styles.icons} data-testid="not-rtl-icons">
-                    <GamepadIcon />
-                    <MagnifierIcon />
-                    <ClockIcon />
-                </div>
-            </section>
+                    </h2>
 
-            <section className={styles.section}>
-                <h2>Заключение</h2>
+                    <p>
+                        <FormattedMessage
+                            id="articleRtlIcons.flipIcons.text"
+                            defaultMessage="Разворачивайте иконки, которые имеют явное направление или
+                        асимметричную форму (за исключением примеров, которые мы
+                        рассмотрим позже в статье). Например, иконка заметок с явным
+                        направлением текста, асимметричная иконка пользовательского
+                        интерфейса окна приложения или иконка сообщения. Примеры
+                        таких иконок:"
+                            description="содержимое раздела статьи"
+                        />
+                    </p>
 
-                <p>
-                    Грамотная работа с иконками в RTL-интерфейсах — важная часть
-                    локализации. Это не просто техническая деталь, а способ
-                    сделать интерфейс по-настоящему удобным для пользователей с
-                    разными культурными особенностями.
-                </p>
+                    <div className={cn(styles.icons, styles.iconsRtl)} data-testid="rtl-icons">
+                        <PaperNoteIcon />
+                        <MessageIcon />
+                        <WindowIcon />
+                    </div>
+                </section>
 
-                <p>
-                    <strong>Рекомендация:</strong> Разработайте внутренние
-                    стандарты для дизайнеров и разработчиков, чтобы обеспечить
-                    единый подход к работе с иконками во всех локализациях
-                    продукта.
-                </p>
-            </section>
-        </main>
-    </Layout>
+                <section className={styles.section}>
+                    <h2>
+                        <FormattedMessage
+                            id="articleRtlIcons.dontFlipIcons.title"
+                            defaultMessage="Иконки, которые не нужно разворачивать"
+                            description="подзаголовок раздела статьи"
+                        />
+                    </h2>
+
+                    <p>
+                        <FormattedMessage
+                            id="articleRtlIcons.dontFlipIcons.text"
+                            defaultMessage="Можно не разворачивать симметричные иконки. Иконки, которые
+                        являются логотипом или представляют бренд тоже не следует
+                        зеркалить — они должны оставаться узнаваемыми. Особое
+                        внимание уделите элементам, которые подчиняются «правилу
+                        правой руки» — если иконка интуитивно ассоциируется с
+                        действием правой руки (как держание предмета), её ориентация
+                        должна сохраняться независимо от направления письма. Примеры
+                        таких иконок:"
+                            description="содержимое раздела статьи"
+                        />
+
+                    </p>
+
+                    <div className={styles.icons} data-testid="not-rtl-icons">
+                        <GamepadIcon />
+                        <MagnifierIcon />
+                        <ClockIcon />
+                    </div>
+                </section>
+
+                <section className={styles.section}>
+                    <h2>
+                        <FormattedMessage
+                            id="articleRtlIcons.conclusion.title"
+                            defaultMessage="Заключение"
+                            description="подзаголовок раздела статьи"
+                        />
+                    </h2>
+
+                    <p>
+                        <FormattedMessage
+                            id="articleRtlIcons.conclusion.text1"
+                            defaultMessage="Грамотная работа с иконками в RTL-интерфейсах — важная часть
+                        локализации. Это не просто техническая деталь, а способ
+                        сделать интерфейс по-настоящему удобным для пользователей с
+                        разными культурными особенностями."
+                            description="содержимое раздела статьи"
+                        />
+
+                    </p>
+
+                    <p>
+                        <FormattedMessage
+                            id="articleRtlIcons.conclusion.text2"
+                            defaultMessage="<strong>Рекомендация:</strong> Разработайте внутренние
+                        стандарты для дизайнеров и разработчиков, чтобы обеспечить
+                        единый подход к работе с иконками во всех локализациях
+                        продукта."
+                            description="содержимое раздела статьи"
+                            values={{
+                                strong: chunks => <strong>{chunks}</strong>
+                            }}
+                        />
+
+                    </p>
+                </section>
+            </main>
+        </Layout>
+    </PageIntlProvider>
 );
